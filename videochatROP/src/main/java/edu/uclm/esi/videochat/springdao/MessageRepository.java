0020949cd78add912b384e0a345ac6ca0e281e93
4paloms <antonio.rubio5@alu.uclm.es>
2021-01-11 19:06:48 +0100

    cleaning

--- videochatROP/src/main/java/edu/uclm/esi/videochat/springdao/MessageRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.uclm.esi.videochat.springdao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import edu.uclm.esi.videochat.model.Message;
-
-public interface MessageRepository extends CrudRepository <Message, String> {
-
-}
--- videochatROP/src/main/webapp/js/Mensaje.js
+++
@@ -1,6 +0,0 @@
-class Mensaje {
-	constructor(texto, hora) {
-		this.texto = texto;
-		this.hora = hora ? hora : Date.now();
-	}
-}
